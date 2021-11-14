2e54af09ecaebd05254f61ea68351872b670636b
farzanapomy <farzanapomy56@gmail.com>
2021-11-15 01:40:11 +0600

    updated

--- src/pages/DashBoard/MakeAdmin/MakeAdmin.js
+++ src/pages/DashBoard/MakeAdmin/MakeAdmin.js
@@ -7,7 +7,7 @@ const MakeAdmin = () => {
 
     const onSubmit = (email) => {
         const user = { email }
-        fetch('http://localhost:5000/users/makeAdmin', {
+        fetch('https://powerful-ravine-08255.herokuapp.com/users/makeAdmin', {
             method: "PUT",
             headers: {
                 "content-type": "application/json"
