dc92a2a1d6ffba65d89d53bf1797fa99439c0fab
farzanapomy <farzanapomy56@gmail.com>
2021-11-30 03:43:00 +0600

    added

--- src/pages/Login/Firebase/firebase.config.js
+++ src/pages/Login/Firebase/firebase.config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDIIFwzUVDfKz74S04QmlhHUJxBQRczHqs",
-  authDomain: "mr-warm-puppies.firebaseapp.com",
-  projectId: "mr-warm-puppies",
-  storageBucket: "mr-warm-puppies.appspot.com",
-  messagingSenderId: "416488098867",
-  appId: "1:416488098867:web:3d69a35026e341cb4de746"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 export default firebaseConfig;
